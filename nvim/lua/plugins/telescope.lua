e41347d62bbe39dd5475700881f9a4c7f63c9548
Sakolkiat <sakolkiat.n@gmail.com>
2025-09-12 16:33:38 +0700

    Config telescope plugin

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,13 +7,35 @@ return {
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
+      require('telescope').setup {
+        pickers = {
+          find_files = {
+            -- theme = "ivy"
+            -- theme = "cursor"
+            theme = "dropdown"
+          }
+        },
+        extensions = {
+          fzf = {}
+        }
+      }
+
+      require("telescope").load_extension('fzf')
+
+      vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
       vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
       vim.keymap.set("n", "<space>en", function()
         require('telescope.builtin').find_files {
           cwd = vim.fn.stdpath("config")
         }
       end)
+      vim.keymap.set("n", "<space>ep", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+        }
+      end)
+
+      require "config.telescope.multigrep".setup()
     end
   }
-
 }

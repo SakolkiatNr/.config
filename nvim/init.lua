b951cd8747ebc33ce07eaf843ec00cc0b4e47358
Sakolkiat <sakolkiat.n@gmail.com>
2025-09-29 23:23:07 +0700

    Configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,8 +13,8 @@ vim.cmd("colorscheme vague")
 -- core
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save" })
 vim.keymap.set("n", "<leader>ee", "<cmd>Oil<CR>", { desc = "Explore" })
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", { desc = "reload config" })
-vim.keymap.set("n", "<space>x", ":.lua<CR>", { desc = "Execute current lua command" })
+vim.keymap.set("n", "<leader><space>x", "<cmd>source %<CR>", { desc = "reload config" })
+vim.keymap.set("n", "<leader>x", ":.lua<CR>", { desc = "Execute current lua command" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic" })
 
 -- window navigation
@@ -27,16 +27,10 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 vim.keymap.set("n", "∆", "<cmd>cnext<CR>")
 vim.keymap.set("n", "˚", "<cmd>cprev<CR>")
 
--- telescope
--- vim.keymap.set("n", "<space>ff", ":Telescope<CR>", { desc = "Open Telescope" })
--- vim.keymap.set("n", "<space>fh", require("telescope.builtin").help_tags, { desc = "Search help" })
--- vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files, { desc = "Search directories" })
--- vim.keymap.set("n", "<space>fb", require("telescope.builtin").buffers, { desc = "Search active buffers" })
-
 -- fugitive (git)
-vim.keymap.set("n", "<leader>ga", ":G add .<CR>", { desc = "Git add ." })
-vim.keymap.set("n", "<leader>gl", ":G log<CR>", { desc = "Git log" })
-vim.keymap.set("n", "<leader>gs", ":G status<CR>", { desc = "Git status" })
+-- vim.keymap.set("n", "<leader>ga", ":G add .<CR>", { desc = "Git add ." })
+-- vim.keymap.set("n", "<leader>gl", ":G log<CR>", { desc = "Git log" })
+-- vim.keymap.set("n", "<leader>gs", ":G status<CR>", { desc = "Git status" })
 
 -- comment
 vim.keymap.set("n", "<leader>/", require("Comment.api").toggle.linewise.current, { desc = "Toggle comment" })
--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,8 @@
 return {
   "tpope/vim-fugitive",
+  config = function()
+    vim.keymap.set("n", "<leader>ga", ":G add .<CR>", { desc = "Git add ." })
+    vim.keymap.set("n", "<leader>gl", ":G log<CR>", { desc = "Git log" })
+    vim.keymap.set("n", "<leader>gs", ":G status<CR>", { desc = "Git status" })
+  end,
 }

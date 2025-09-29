860bc5fc313b31ebd8b8dc044303934b91ad130d
Sakolkiat <sakolkiat.n@gmail.com>
2025-09-30 03:51:11 +0700

    Configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,26 @@
+require("config.lazy")
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.scrolloff = 5
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.clipboard = "unnamedplus"
+vim.opt.winborder = "rounded"
+vim.cmd("colorscheme vague")
+
+-- core
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save" })
+vim.keymap.set("n", "<leader>ee", "<cmd>Oil<CR>", { desc = "Explore" })
+vim.keymap.set("n", "<leader><space>x", "<cmd>source %<CR>", { desc = "reload config" })
+vim.keymap.set("n", "<leader>x", ":.lua<CR>", { desc = "Execute current lua command" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic" })
+
+-- window navigation
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 
 -- quick fix navigation

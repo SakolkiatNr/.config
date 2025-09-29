9c81964ce33af1ed2bb408b238503fe39876b44f
Sakolkiat <sakolkiat.n@gmail.com>
2025-09-30 03:47:28 +0700

    Configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,43 +1,9 @@
-require("config.lazy")
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.scrolloff = 5
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.clipboard = "unnamedplus"
-vim.opt.winborder = "rounded"
-vim.cmd("colorscheme vague")
-
--- core
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save" })
-vim.keymap.set("n", "<leader>ee", "<cmd>Oil<CR>", { desc = "Explore" })
-vim.keymap.set("n", "<leader><space>x", "<cmd>source %<CR>", { desc = "reload config" })
-vim.keymap.set("n", "<leader>x", ":.lua<CR>", { desc = "Execute current lua command" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic" })
-
--- window navigation
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 
 -- quick fix navigation
 vim.keymap.set("n", "∆", "<cmd>cnext<CR>")
 vim.keymap.set("n", "˚", "<cmd>cprev<CR>")
 
--- comment
-vim.keymap.set("n", "<leader>/", require("Comment.api").toggle.linewise.current, { desc = "Toggle comment" })
-
--- Highlight when yanking (copy) text
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking text",
-  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
 require("config.telescope.terminal")
 require("config.telescope.timew")
+require("config.telescope.hlyank")
--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -4,5 +4,9 @@ return {
     opts = {
       -- add any options here
     },
+    config = function(_, opts)
+      require("Comment").setup(opts)
+      vim.keymap.set("n", "<leader>/", require("Comment.api").toggle.linewise.current, { desc = "Toggle comment" })
+    end,
   },
 }
